345fed5e7284759d74c407d4786c6c9d19d55779
Tony Dam <thonydam@gmail.com>
2018-10-15 21:00:43 -0700

    Init commit

---
+++ GithubGraphQL/ViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+import Apollo
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    //Initial query
+    let gqlQuery = SearchRepositoriesQuery.init(first: 5, query: "graphql", type: SearchType.repository)
+
+    //Paginated search Query
+    //let gqlQuery = SearchRepositoriesQuery.init(first: 5, after: "Y3Vyc29yOjQ=", query: "graphql", type: SearchType.repository)
+    
+    RepositoriesGraphQLClient.searchRepositories(query: gqlQuery) { (result) in
+      switch result {
+      case .success(let data):
+        if let gqlResult = data {
+          
+          if let pageInfo = gqlResult.data?.search.pageInfo {
+            print("pageInfo: \n")
+            print("hasNextPage: \(pageInfo.hasNextPage)")
+            print("hasPreviousPage: \(pageInfo.hasPreviousPage)")
+            print("startCursor: \(String(describing: pageInfo.startCursor))")
+            print("endCursor: \(String(describing: pageInfo.endCursor))")
+            print("\n")
+          }
+          
+          
+          gqlResult.data?.search.edges?.forEach { edge in
+            guard let repository = edge?.node?.asRepository?.fragments.repositoryDetails else { return }
+            print("Name: \(repository.name)")
+            print("Path: \(repository.url)")
+            print("Owner: \(repository.owner.login)")
+            print("avatar: \(repository.owner.avatarUrl)")
+            print("Stars: \(repository.stargazers.totalCount)")
+            print("\n")
+          }
+        }
+      case .failure(let error):
+        if let error = error {
+          print(error)
+        }
+      }
+    }
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+}
